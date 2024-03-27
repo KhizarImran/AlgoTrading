ec694a46623f20a50c991c64fb44a19a35b04b95
Khizar <khzrimrn@gmail.com>
2024-03-27 05:01:33 +0000

    corrected logic

--- Quant_Research/Martingale_XAU.py
+++ Quant_Research/Martingale_XAU.py
@@ -3,12 +3,11 @@ import pandas as pd
 import time
 from datetime import datetime
 
-
 def connect_to_mt5():
     if not mt5.initialize():
         print("Failed to initialize the Metatrader 5 library.")
         return False
-    print("Connecteed to MetaTrader 5")
+    print("Connected to MetaTrader 5")
     return True
 
 def get_data(symbol, timeframe, start_date, end_time):
@@ -19,23 +18,24 @@ def get_data(symbol, timeframe, start_date, end_time):
     return df
 
 def get_sma(data, period):
-    sma = data['close'].rolling(period).mean()
+    calc_sma = data['close'].tail(period)
+    sma = calc_sma.mean()
     return sma
 
+def get_prev_sma(data, period):
+    prev_sma = data['close'].rolling(period).mean()
+    return prev_sma
 
 def market_order(symbol, volume, order_type, deviation=20, magic=261200):
-    
     order_type_dict = {
         'buy'  : mt5.ORDER_TYPE_BUY,
         'sell'  : mt5.ORDER_TYPE_SELL
     }
-
     price_dict = {
         'buy' : mt5.symbol_info_tick(symbol).ask,
         'sell' : mt5.symbol_info_tick(symbol).bid
     }
     
-    # Calculate stop loss and take profit levels based on price and pips
     if order_type == 'buy':
         sl_price = price_dict['buy'] - (200 * mt5.symbol_info(symbol).point)
         tp_price = price_dict['buy'] + (500 * mt5.symbol_info(symbol).point)
@@ -53,22 +53,20 @@ def market_order(symbol, volume, order_type, deviation=20, magic=261200):
         "price": price_dict[order_type],
         "sl": sl_price,  # FLOAT
         "tp": tp_price,  # FLOAT
-        "deviation": deviation,  # INTERGER
-        "magic": magic,  # INTERGER
+        "deviation": deviation,  # INTEGER
+        "magic": magic,  # INTEGER
         "comment": "my_first_strat",
         "type_time": mt5.ORDER_TIME_GTC,
         "type_filling": mt5.ORDER_FILLING_IOC,
     }
     
     order_result = mt5.order_send(request)
-    return(order_result)
+    return order_result
 
 def main():
-
     symbol = 'XAUUSD'
-    timeframe = mt5.TIMEFRAME_H1
+    timeframe = mt5.TIMEFRAME_M15
     volume = 0.1
-    
     
     if not connect_to_mt5():
         return
@@ -81,8 +79,8 @@ def main():
               '| Equity: ' , account_info.equity,
               '| Profit: ', account_info.profit)
         
-        current_time = int(time.time())
-        start_time = datetime(2022, 8, 1)
+        current_time = datetime.now()
+        start_time = datetime(2024, 1, 1)
         end_time = current_time
 
         data = get_data(symbol, timeframe, start_time, end_time)
@@ -91,29 +89,35 @@ def main():
             time.sleep(60)
             continue
         
-        # Initiating strategy
         fast_ma = get_sma(data,  20)
         slow_ma = get_sma(data, 120)
+        
 
-        in_position = False
+
+        # Check if there are open positions
+        positions_total = mt5.positions_total()
         
+        # Keep track of current position
+        in_position = positions_total > 0
         
-        for index, row in data.iterrows():
-            if mt5.positions_total() == 0:
-                in_position = False
-            elif not  in_position and fast_ma.loc[index] > slow_ma.loc[index] and fast_ma.loc[index - 1] <= slow_ma.loc[index - 1]:
+        prev_fast_ma = get_prev_sma(data, 20).iloc[-1]
+        prev_slow_ma = get_prev_sma(data, 120).iloc[-1]
+
+
+        # Check conditions and place trades
+        if not in_position: 
+            if (fast_ma > slow_ma) and (prev_fast_ma <= prev_slow_ma):
                 market_order(symbol, volume, 'buy')
-                print('Entered Short Position')
+                print('Buy signal detected')
                 in_position = True
-            elif not in_position and fast_ma.loc[index] < slow_ma.loc[index] and fast_ma.loc[index - 1] >= slow_ma.loc[index - 1]:
+
+            elif (fast_ma < slow_ma) and (prev_fast_ma >= prev_slow_ma):
                 market_order(symbol, volume, 'sell')
-                print('Entered short position')
+                print('Sell signal detected')
                 in_position = True
             
-            
-        time.sleep(900)  # Check every 15 minute
+        
+        time.sleep(900)  # Check every 15 minutes
 
 if __name__ == "__main__":
     main()
-    
-    
