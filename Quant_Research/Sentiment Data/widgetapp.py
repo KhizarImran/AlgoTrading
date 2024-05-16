fb9c4d01d4c2f62e1c7d8d5bbbac4d3a8852483d
Khizar <khzrimrn@gmail.com>
2024-05-16 17:07:51 +0100

    developing a testing website for COT Dashboard

--- Quant_Research/Sentiment Data/widgetapp.py
+++ Quant_Research/Sentiment Data/widgetapp.py
@@ -1,18 +1,26 @@
 import streamlit as st
 
+# Set the page layout to wide
+st.set_page_config(layout="wide")
+
 def embed_myfxbook_widget():
-    st.markdown(
-        """
-        <iframe src="https://widgets.myfxbook.com/widgets/fxOutlook.html?type=0&symbols=,1,2,3,4,5,6,7,8,9,10,11,12,13,14,17,20,24,25,26,27,28,29,46,47,48,49,51,103,107" 
-                width="100%" 
-                height="600" 
-                frameborder="0" 
-                scrolling="no" 
-                style="border:none;">
-        </iframe>
-        """,
-        unsafe_allow_html=True
-    )
+    html_code = """
+    <div>
+        <script class="powered" type="text/javascript"
+                src="https://widgets.myfxbook.com/scripts/fxOutlook.js?type=1&symbols=,1,2,3,4,5,6,7,8,9,10,11,12,13,14,17,20,24,25,26,27,28,29,46,47,48,49,51,103,107"></script>
+    </div>
+    <div style="font-size: 10px">
+        <a href="https://www.myfxbook.com" title="" class="myfxbookLink" target="_self" rel="noopener">
+            Powered by Myfxbook.com
+        </a>
+    </div>
+
+    <script type="text/javascript">
+        showOutlookWidget()
+    </script>
+    """
+
+    st.components.v1.html(html_code)
 
 def main():
     st.title('Myfxbook Outlook Widget')
